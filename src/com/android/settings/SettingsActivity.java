986c78318e5cb6a553e59fb5ee9a51887654ddc7
Danny Baumann <dannybaumann@web.de>
2014-11-12 17:14:27 -0800

    Add back blacklist settings.
    Change-Id: Iae35717acfa9af4da75c07d24352acf981d19fc7
    
    Improve translatability of summaries.
    
    Change-Id: I362b335cbe219d6e371a458c9d50fcd66bdf19fe
    
    Settings: Alert user when they attempt to add an invalid number to BL.
    
      - BlacklistProvider will reject numbers that aren't valid, silently. So instead of
      resuming to a blank screen, we should alert the user that the number they attempted to
      add is invalid by utilizing BlacklistUtils#addOrUpdate method.
    
    Change-Id: I1ca32427baad41c1c9379e18046e6ca7937b6d32
    
    Add missing blacklist xxhdpi drawables.
    
    Change-Id: I327f408ff344e9358c479cf5fe9dc341918511b2
    
    Blacklist: Fix launching blacklist settings
    
    Change-Id: I0043503e6ac0d3536941394ee5057001b72f9114
    
    Settings : Launch only contacts picker for blacklist entry
    
    Change-Id: I266d1ad7f1aab745728b33490268cb197188f2b3
    
    Settings: Get blacklist up and running
    
    Most of this commit (https://github.com/BenzoRom/packages_apps_Settings/commit/fc3bafaf0c3f52bcebcc35ccdeb8dd18f426925c) is from:
    
    privacy: Actually set the blacklist preference
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/9897751d02be1f51caeb5a8d9647c1f8ae7a03ff
    
    Move Blacklist and Notif Filter to Privacy
    https://github.com/CyanogenMod/android_packages_apps_Settings/commit/1bf1d26827d35dca4564c4195bf86bc629c1e93b
    
    I (@mydongistiny) just made it work without CM's Profiles and some other CM stuff.
    The SMS option to block is still in there.
    
    Signed-off-by: Chet Kener <Cl3Kener@gmail.com>
    
    Conflicts:
    	res/drawable/ic_settings_appops.xml
    	res/values/custom_arrays.xml
    	res/values/custom_strings.xml
    	res/xml/dashboard_categories.xml
    	src/com/android/settings/Settings.java
    	src/com/android/settings/SettingsActivity.java

--- src/com/android/settings/SettingsActivity.java
+++ src/com/android/settings/SettingsActivity.java
@@ -79,6 +79,7 @@ import com.android.settings.applications.ProcessStatsSummary;
 import com.android.settings.applications.ProcessStatsUi;
 import com.android.settings.applications.UsageAccessDetails;
 import com.android.settings.applications.WriteSettingsDetails;
+import com.android.settings.blacklist.BlacklistSettings;
 import com.android.settings.bluetooth.BluetoothSettings;
 import com.android.settings.nitrogen.DisplayRotation;
 import com.android.settings.dashboard.DashboardCategory;
@@ -357,7 +358,9 @@ public class SettingsActivity extends Activity
             DrawOverlayDetails.class.getName(),
             WriteSettingsDetails.class.getName(),
             DisplayRotation.class.getName(),
-            ExpandedDesktopPreferenceFragment.class.getName()
+            ExpandedDesktopPreferenceFragment.class.getName(),
+            com.android.settings.cyanogenmod.PrivacySettings.class.getName(),
+            BlacklistSettings.class.getName()
     };
 
 
