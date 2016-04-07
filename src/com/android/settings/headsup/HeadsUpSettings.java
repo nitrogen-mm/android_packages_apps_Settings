AICP/packages_apps_Settings

Heads Up Snooze 1/2
Change-Id: Iedf492f284c1d547037ee54aa3b57b461eb0839e

src/com/android/settings/headsup/HeadsUpSettings.java
@@ -55,8 +55,10 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     private static final int DIALOG_WHITELIST_APPS = 2;
 
     private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
+    private static final String HEADS_UP_SNOOZE_LENGTH_MS = "heads_up_snooze_length_ms";
 
     private ListPreference mHeadsUpTimeOut;
+    private ListPreference mHeadsUpSnooze;
 
     private PackageListAdapter mPackageAdapter;
     private PackageManager mPackageManager;
@@ -97,6 +99,15 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
                 Settings.System.HEADS_UP_TIMEOUT, defaultTimeOut);
         mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
         updateHeadsUpTimeOutSummary(headsUpTimeOut);
+
+        int defaultSnooze = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_default_snooze_length_ms", null, null));
+        mHeadsUpSnooze = (ListPreference) findPreference(HEADS_UP_SNOOZE_LENGTH_MS);
+        mHeadsUpSnooze.setOnPreferenceChangeListener(this);
+        int headsUpSnooze = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_SNOOZE_LENGTH_MS, defaultSnooze);
+        mHeadsUpSnooze.setValue(String.valueOf(headsUpSnooze));
+        updateHeadsUpSnoozeSummary(headsUpSnooze);
 
         mDndPrefList = (PreferenceGroup) findPreference("dnd_applications_list");
         mDndPrefList.setOrderingAsAdded(false);
@@ -290,6 +301,13 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
                     headsUpTimeOut);
             updateHeadsUpTimeOutSummary(headsUpTimeOut);
             return true;
+        } else if (preference == mHeadsUpSnooze) {
+            int headsUpSnooze = Integer.valueOf((String) objValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_SNOOZE_LENGTH_MS,
+                    headsUpSnooze);
+            updateHeadsUpSnoozeSummary(headsUpSnooze);
+            return true;
         }
         return false;
     }
@@ -298,6 +316,12 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         String summary = getResources().getString(R.string.heads_up_time_out_summary,
                 value / 1000);
         mHeadsUpTimeOut.setSummary(summary);
+    }
+
+    private void updateHeadsUpSnoozeSummary(int value) {
+        String summary = getResources().getString(R.string.heads_up_snooze_summary,
+                value / 60000);
+        mHeadsUpSnooze.setSummary(summary);
     }
 
     @Override

nitrogen-mm/android_packages_apps_Settings

src/com/android/settings/headsup/HeadsUpSettings.java
@@ -52,8 +52,10 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
     private static final int DIALOG_WHITELIST_APPS = 0;
 
     private static final String PREF_HEADS_UP_TIME_OUT = "heads_up_time_out";
+    private static final String HEADS_UP_SNOOZE_LENGTH_MS = "heads_up_snooze_length_ms";
 
     private ListPreference mHeadsUpTimeOut;
+    private ListPreference mHeadsUpSnooze;
 
     private PackageListAdapter mPackageAdapter;
     private PackageManager mPackageManager;
@@ -86,6 +88,15 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
                 Settings.System.HEADS_UP_TIMEOUT, defaultTimeOut);
         mHeadsUpTimeOut.setValue(String.valueOf(headsUpTimeOut));
         updateHeadsUpTimeOutSummary(headsUpTimeOut);
+
+        int defaultSnooze = systemUiResources.getInteger(systemUiResources.getIdentifier(
+                    "com.android.systemui:integer/heads_up_default_snooze_length_ms", null, null));
+        mHeadsUpSnooze = (ListPreference) findPreference(HEADS_UP_SNOOZE_LENGTH_MS);
+        mHeadsUpSnooze.setOnPreferenceChangeListener(this);
+        int headsUpSnooze = Settings.System.getInt(getContentResolver(),
+                Settings.System.HEADS_UP_SNOOZE_LENGTH_MS, defaultSnooze);
+        mHeadsUpSnooze.setValue(String.valueOf(headsUpSnooze));
+        updateHeadsUpSnoozeSummary(headsUpSnooze);
 
         mWhitelistPrefList = (PreferenceGroup) findPreference("whitelist_applications");
         mWhitelistPrefList.setOrderingAsAdded(false);
@@ -207,6 +218,13 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
                     headsUpTimeOut);
             updateHeadsUpTimeOutSummary(headsUpTimeOut);
             return true;
+        } else if (preference == mHeadsUpSnooze) {
+            int headsUpSnooze = Integer.valueOf((String) newValue);
+            Settings.System.putInt(getContentResolver(),
+                    Settings.System.HEADS_UP_SNOOZE_LENGTH_MS,
+                    headsUpSnooze);
+            updateHeadsUpSnoozeSummary(headsUpSnooze);
+            return true;
         }
         return false;
     }
@@ -215,6 +233,12 @@ public class HeadsUpSettings extends SettingsPreferenceFragment
         String summary = getResources().getString(R.string.heads_up_time_out_summary,
                 value / 1000);
         mHeadsUpTimeOut.setSummary(summary);
+    }
+
+    private void updateHeadsUpSnoozeSummary(int value) {
+        String summary = getResources().getString(R.string.heads_up_snooze_summary,
+                value / 60000);
+        mHeadsUpSnooze.setSummary(summary);
     }
 
     @Override